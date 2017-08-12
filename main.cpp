6f947e6df98b91ba9e6bd23ad628ebadb880c749
HaijianLiu <haijanliu@gmail.com>
2017-08-12 11:05:07 +0900

    debug

--- main.cpp
+++ main.cpp
@@ -1,55 +1,25 @@
 ﻿
-#define _CRT_SECURE_NO_WARNINGS // scanf のwarning防止
-#include "windows.h"
-#include "d3dx9.h"
-#include <tchar.h>
 
-#define DIRECTINPUT_VERSION 0x0800 // 警告対処
-#include "dinput.h"
-#include "mmsystem.h"
-
-#if 1 // [ここを"0"にした場合、"構成プロパティ" -> "リンカ" -> "入力" -> "追加の依存ファイル"に対象ライブラリを設定する]
-#pragma comment (lib, "d3d9.lib")
-#pragma comment (lib, "d3dx9.lib")
-#pragma comment (lib, "dinput8.lib")
-#pragma comment (lib, "dxguid.lib")
-#pragma comment (lib, "winmm.lib")
-#endif
-
-#define SCREEN_WIDTH    (640)                 // ウインドウの幅
-#define SCREEN_HEIGHT   (480)                 // ウインドウの高さ
-#define SCREEN_CENTER_X (SCREEN_WIDTH  / 2)   // ウインドウの中心Ｘ座標
-#define SCREEN_CENTER_Y (SCREEN_HEIGHT / 2)   // ウインドウの中心Ｙ座標
-
-// 頂点フォーマット( 頂点座標[2D] / 反射光 / テクスチャ座標 )
-#define FVF_VERTEX_2D (D3DFVF_XYZRHW | D3DFVF_DIFFUSE | D3DFVF_TEX1)
-
-// 上記頂点フォーマットに合わせた構造体を定義
-typedef struct {
-	D3DXVECTOR3 vtx;  // 頂点座標
-	float rhw;        // テクスチャのパースペクティブコレクト用
-	D3DCOLOR diffuse; // 反射光
-	D3DXVECTOR2 tex;  // テクスチャ座標(*33)
-} VERTEX_2D;
-
-
-LPDIRECT3DDEVICE9 GetDevice(void); // デバイス取得関数
+#include "main.h"
+#include "player.h"
 
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
 #define CLASS_NAME  _T("AppClass")     // ウインドウのクラス名
-#define WINDOW_NAME _T("vania") // ウインドウのキャプション名
+#define WINDOW_NAME _T("アニメーション") // ウインドウのキャプション名
 
+//*****************************************************************************
+// 構造体定義
+//*****************************************************************************
 //*****************************************************************************
 // プロトタイプ宣言
 //*****************************************************************************
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 HRESULT Init(HWND hWnd, BOOL bWindow);
-
+void Uninit(void);
 void Update(void);
 void Draw(void);
-void Uninit(void);
 
 //*****************************************************************************
 // グローバル変数:
@@ -58,9 +28,9 @@ LPDIRECT3D9       g_pD3D = NULL;       // Direct3Dオブジェクト
 LPDIRECT3DDEVICE9 g_pD3DDevice = NULL; // Deviceオブジェクト(描画に必要)
 
 #ifdef _DEBUG // (*35)
-	LPD3DXFONT g_pD3DXFont = NULL; // フォントへのポインタ
-	int        g_nCountFPS; // FPSカウンタ
-	void       DrawDebugFont(void);
+LPD3DXFONT g_pD3DXFont = NULL; // フォントへのポインタ
+int        g_nCountFPS; // FPSカウンタ
+void DrawDebugFont(void);
 #endif
 
 
@@ -78,23 +48,42 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	DWORD dwCurrentTime;
 	DWORD dwFrameCount;
 
-	// window class
-	WNDCLASSEX wcex = {sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0, 0, hInstance, NULL, LoadCursor(NULL, IDC_ARROW), (HBRUSH)(COLOR_WINDOW+1), NULL, CLASS_NAME, NULL};
-	HWND hWnd;
-	MSG  msg;
+	WNDCLASSEX	wcex = {
+		sizeof(WNDCLASSEX),
+		CS_CLASSDC,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		NULL,
+		LoadCursor(NULL, IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW+1),
+		NULL,
+		CLASS_NAME,
+		NULL
+	};
+	HWND		hWnd;
+	MSG			msg;
+
 	// ウィンドウクラスの登録
 	RegisterClassEx(&wcex);
 
 	// ウィンドウの作成
-	hWnd = CreateWindow(CLASS_NAME, WINDOW_NAME, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, // ウィンドウの左座標
-		CW_USEDEFAULT, // ウィンドウの上座標
-		SCREEN_WIDTH + GetSystemMetrics(SM_CXDLGFRAME)*2, // ウィンドウ横幅
-		SCREEN_HEIGHT + GetSystemMetrics(SM_CXDLGFRAME)*2 + GetSystemMetrics(SM_CYCAPTION), // ウィンドウ縦幅
-		NULL, NULL, hInstance, NULL);
+	hWnd = CreateWindow(CLASS_NAME,
+						WINDOW_NAME,
+						WS_OVERLAPPEDWINDOW,
+						CW_USEDEFAULT,																		// ウィンドウの左座標
+						CW_USEDEFAULT,																		// ウィンドウの上座標
+						SCREEN_WIDTH + GetSystemMetrics(SM_CXDLGFRAME)*2,									// ウィンドウ横幅
+						SCREEN_HEIGHT + GetSystemMetrics(SM_CXDLGFRAME)*2+GetSystemMetrics(SM_CYCAPTION),	// ウィンドウ縦幅
+						NULL,
+						NULL,
+						hInstance,
+						NULL);
 
 	// DirectXの初期化(ウィンドウを作成してから行う)
-	if(FAILED(Init(hWnd, true))) {
+	if(FAILED(Init(hWnd, true)))
+	{
 		return -1;
 	}
 
@@ -106,30 +95,31 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	// ウインドウの表示(Init()の後に呼ばないと駄目)
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
-	// InitInput(hInstance,hWnd);
 
 	// メッセージループ
 	while(1)
 	{
-		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-			if(msg.message == WM_QUIT) {// PostQuitMessage()が呼ばれたらループ終了
+		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if(msg.message == WM_QUIT)
+			{// PostQuitMessage()が呼ばれたらループ終了
 				break;
 			}
-			else {
+			else
+			{
 				// メッセージの翻訳とディスパッチ
 				TranslateMessage(&msg);
 				DispatchMessage(&msg);
 			}
 		}
-		else {
+		else
+		{
 			dwCurrentTime = timeGetTime();
 
 			if ((dwCurrentTime - dwFPSLastTime) >= 500) {
-
-				#ifdef _DEBUG
-					g_nCountFPS = (dwFrameCount * 1000) / (dwCurrentTime - dwFPSLastTime); // FPSを計測
-				#endif
-
+#ifdef _DEBUG
+				g_nCountFPS = (dwFrameCount * 1000) / (dwCurrentTime - dwFPSLastTime); // FPSを計測
+#endif
 				dwFPSLastTime = dwCurrentTime; // FPS計測時刻を保存
 				dwFrameCount = 0; // カウントをクリア
 			}
@@ -142,7 +132,9 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 				Draw();
 				dwFrameCount ++; // 処理回数のカウントを加算
 			}
+
 		}
+
 	}
 
 	// ウィンドウクラスの登録を解除
@@ -187,69 +179,74 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 HRESULT Init(HWND hWnd, BOOL bWindow)
 {
 	D3DPRESENT_PARAMETERS d3dpp;
-	D3DDISPLAYMODE        d3ddm;
+	D3DDISPLAYMODE d3ddm;
 
 	// Direct3Dオブジェクトの作成
 	g_pD3D = Direct3DCreate9(D3D_SDK_VERSION);
-	if(g_pD3D == NULL) {
+	if(g_pD3D == NULL)
+	{
 		return E_FAIL;
 	}
 
 	// 現在のディスプレイモードを取得
-	if(FAILED(g_pD3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &d3ddm))) {
+	if(FAILED(g_pD3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &d3ddm)))
+	{
 		return E_FAIL;
 	}
 
 	// デバイスのプレゼンテーションパラメータの設定
-	ZeroMemory(&d3dpp, sizeof(d3dpp)); // ワークをゼロクリア
-	d3dpp.BackBufferCount        = 1; // バックバッファの数
-	d3dpp.BackBufferWidth        = SCREEN_WIDTH; // ゲーム画面サイズ(幅)
-	d3dpp.BackBufferHeight       = SCREEN_HEIGHT; // ゲーム画面サイズ(高さ)
-	d3dpp.BackBufferFormat       = D3DFMT_UNKNOWN; // バックバッファのフォーマットは現在設定されているものを使う
-	d3dpp.SwapEffect             = D3DSWAPEFFECT_DISCARD; // 映像信号に同期してフリップする
-	d3dpp.Windowed               = bWindow; // ウィンドウモード
-	d3dpp.EnableAutoDepthStencil = TRUE; // デプスバッファ（Ｚバッファ）とステンシルバッファを作成
-	d3dpp.AutoDepthStencilFormat = D3DFMT_D16; // デプスバッファとして16bitを使う
-	d3dpp.BackBufferFormat       = d3ddm.Format; // カラーモードの指定
+	ZeroMemory(&d3dpp, sizeof(d3dpp));							// ワークをゼロクリア
+	d3dpp.BackBufferCount			= 1;						// バックバッファの数
+	d3dpp.BackBufferWidth			= SCREEN_WIDTH;				// ゲーム画面サイズ(幅)
+	d3dpp.BackBufferHeight			= SCREEN_HEIGHT;			// ゲーム画面サイズ(高さ)
+	d3dpp.BackBufferFormat			= D3DFMT_UNKNOWN;			// バックバッファのフォーマットは現在設定されているものを使う
+	d3dpp.SwapEffect				= D3DSWAPEFFECT_DISCARD;	// 映像信号に同期してフリップする
+	d3dpp.Windowed					= bWindow;					// ウィンドウモード
+	d3dpp.EnableAutoDepthStencil	= TRUE;						// デプスバッファ（Ｚバッファ）とステンシルバッファを作成
+	d3dpp.AutoDepthStencilFormat	= D3DFMT_D16;				// デプスバッファとして16bitを使う
+	d3dpp.BackBufferFormat			= d3ddm.Format;				// カラーモードの指定
+	d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE;
 
-	if(bWindow) { // ウィンドウモード
-		d3dpp.BackBufferFormat           = D3DFMT_UNKNOWN; // バックバッファ
-		d3dpp.FullScreen_RefreshRateInHz = 0; // リフレッシュレート
-		d3dpp.PresentationInterval       = D3DPRESENT_INTERVAL_IMMEDIATE; // インターバル
+	if(bWindow)
+	{// ウィンドウモード
+		d3dpp.BackBufferFormat           = D3DFMT_UNKNOWN;					// バックバッファ
+		d3dpp.FullScreen_RefreshRateInHz = 0;								// リフレッシュレート
+		d3dpp.PresentationInterval       = D3DPRESENT_INTERVAL_IMMEDIATE;	// インターバル
 	}
-	else {// フルスクリーンモード
-		d3dpp.BackBufferFormat           = D3DFMT_R5G6B5; // バックバッファ
-		d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT; // リフレッシュレート
-		d3dpp.PresentationInterval       = D3DPRESENT_INTERVAL_DEFAULT; // インターバル
+	else
+	{// フルスクリーンモード
+		d3dpp.BackBufferFormat           = D3DFMT_R5G6B5;					// バックバッファ
+		d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;			// リフレッシュレート
+		d3dpp.PresentationInterval       = D3DPRESENT_INTERVAL_DEFAULT;		// インターバル
 	}
 
 	// デバイスの生成
 	// ディスプレイアダプタを表すためのデバイスを作成
 	// 描画と頂点処理をハードウェアで行なう
-	if(FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, // ディスプレイアダプタ
-		D3DDEVTYPE_HAL, // ディスプレイタイプ
-		hWnd, // フォーカスするウインドウへのハンドル
-		D3DCREATE_HARDWARE_VERTEXPROCESSING, // デバイス作成制御の組み合わせ
-		&d3dpp, // デバイスのプレゼンテーションパラメータ
-		&g_pD3DDevice))) // デバイスインターフェースへのポインタ
+	if(FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT,							// ディスプレイアダプタ
+									D3DDEVTYPE_HAL,								// ディスプレイタイプ
+									hWnd,										// フォーカスするウインドウへのハンドル
+									D3DCREATE_HARDWARE_VERTEXPROCESSING,		// デバイス作成制御の組み合わせ
+									&d3dpp,										// デバイスのプレゼンテーションパラメータ
+									&g_pD3DDevice)))							// デバイスインターフェースへのポインタ
 	{
 		// 上記の設定が失敗したら
 		// 描画をハードウェアで行い、頂点処理はCPUで行なう
 		if(FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT,
-			D3DDEVTYPE_HAL,
-			hWnd,
-			D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-			&d3dpp,
-			&g_pD3DDevice)))
+										D3DDEVTYPE_HAL,
+										hWnd,
+										D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+										&d3dpp,
+										&g_pD3DDevice)))
 		{
 			// 上記の設定が失敗したら
 			// 描画と頂点処理をCPUで行なう
 			if(FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT,
-				D3DDEVTYPE_REF,
-				hWnd,
-				D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-				&d3dpp,
-				&g_pD3DDevice)))
+											D3DDEVTYPE_REF,
+											hWnd,
+											D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+											&d3dpp,
+											&g_pD3DDevice)))
 			{
 				// 初期化失敗
 				return E_FAIL;
@@ -258,21 +255,28 @@ HRESULT Init(HWND hWnd, BOOL bWindow)
 	}
 
 	// レンダリングステートパラメータの設定
-	g_pD3DDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE); // カリングを行わない
-	g_pD3DDevice->SetRenderState(D3DRS_ZENABLE, TRUE); // Zバッファを使用
-	g_pD3DDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE); // αブレンドを行う
-	g_pD3DDevice->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA); // αソースカラーの指定
-	g_pD3DDevice->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA); // αデスティネーションカラーの指定
+	g_pD3DDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);				// カリングを行わない
+	g_pD3DDevice->SetRenderState(D3DRS_ZENABLE, TRUE);						// Zバッファを使用
+	g_pD3DDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);				// αブレンドを行う
+	g_pD3DDevice->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);		// αソースカラーの指定
+	g_pD3DDevice->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);	// αデスティネーションカラーの指定
 
 	// サンプラーステートパラメータの設定
-	g_pD3DDevice->SetSamplerState(0, D3DSAMP_ADDRESSU, D3DTADDRESS_WRAP); // テクスチャＵ値の繰り返し設定
-	g_pD3DDevice->SetSamplerState(0, D3DSAMP_ADDRESSV, D3DTADDRESS_WRAP); // テクスチャＶ値の繰り返し設定
-	g_pD3DDevice->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR); // テクスチャ拡大時の補間設定
-	g_pD3DDevice->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR); // テクスチャ縮小時の補間設定
+	g_pD3DDevice->SetSamplerState(0, D3DSAMP_ADDRESSU, D3DTADDRESS_WRAP);	// テクスチャＵ値の繰り返し設定
+	g_pD3DDevice->SetSamplerState(0, D3DSAMP_ADDRESSV, D3DTADDRESS_WRAP);	// テクスチャＶ値の繰り返し設定
+	g_pD3DDevice->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_POINT);	// テクスチャ拡大時の補間設定
+	g_pD3DDevice->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_POINT);	// テクスチャ縮小時の補間設定
+	g_pD3DDevice->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_POINT);	// テクスチャ縮小時の補間設定
+	// g_pD3DDevice->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_NONE);	// テクスチャ拡大時の補間設定
+	// g_pD3DDevice->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_NONE);	// テクスチャ縮小時の補間設定
+
 
 	#ifdef _DEBUG
-		D3DXCreateFont(g_pD3DDevice,18,0,0,0,FALSE,SHIFTJIS_CHARSET,OUT_DEFAULT_PRECIS,DEFAULT_QUALITY,DEFAULT_PITCH,_T("Terminal"),&g_pD3DXFont);
+	D3DXCreateFont(g_pD3DDevice,18,0,0,0,FALSE,SHIFTJIS_CHARSET,OUT_DEFAULT_PRECIS,DEFAULT_QUALITY,DEFAULT_PITCH,_T("Terminal"),&g_pD3DXFont);
 	#endif
+
+	// ポリゴンの初期化処理
+	InitPlayer();
 
 	return S_OK;
 }
@@ -297,10 +301,8 @@ void Uninit(void)
 		g_pD3DXFont->Release();
 	}
 	#endif
-	// UninitInput();
 	// ポリゴンの終了処理
-	// UninitPlayer();
-	// UninitEnemy();
+	UninitPlayer();
 }
 
 //=============================================================================
@@ -308,10 +310,8 @@ void Uninit(void)
 //=============================================================================
 void Update(void)
 {
-	// UpdateInput();
 	// ポリゴンの更新処理
-	// UpdatePlayer();
-	// UpdateEnemy();
+	UpdatePlayer();
 }
 
 //=============================================================================
@@ -325,9 +325,12 @@ void Draw(void)
 	// Direct3Dによる描画の開始
 	if(SUCCEEDED(g_pD3DDevice->BeginScene()))
 	{
+		g_pD3DDevice->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_POINT);	// テクスチャ拡大時の補間設定
+		g_pD3DDevice->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_POINT);	// テクスチャ縮小時の補間設定
+		g_pD3DDevice->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_POINT);	// テクスチャ縮小時の補間設定
+
 		// ポリゴンの描画処理
-		// DrawEnemy();
-		// DrawPlayer();
+		DrawPlayer();
 
 		#ifdef _DEBUG
 		DrawDebugFont(); // 一番最後にDrawするように
--- player.cpp
+++ player.cpp
@@ -1,1 +1,167 @@
 ﻿
+
+#include "main.h"
+#include "player.h"
+
+
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+HRESULT MakeVertexPlayer(void);
+void SetTexturePlayer( int cntPattern );	// (*34)
+void SetVertexPlayer(void);
+
+//*****************************************************************************
+// グローバル変数
+//*****************************************************************************
+LPDIRECT3DTEXTURE9 g_pD3DTexturePlayer = NULL; // テクスチャへのポリゴン (*33)
+
+VERTEX_2D          g_vertexWkPlayer[NUM_VERTEX];      // 頂点情報格納ワーク
+
+D3DXVECTOR3        g_posPlayer;                // ポリゴンの移動量
+D3DXVECTOR3        g_rotPlayer;                // ポリゴンの回転量
+int                g_nCountAnimPlayer;                // アニメーションカウント (*34)
+int                g_nPatternAnimPlayer;              // アニメーションパターンナンバー (*34)
+int                g_dirPlayer = 0;
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT InitPlayer(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+
+	g_posPlayer = D3DXVECTOR3(0.5f, 0.5f, 0.5f);
+	g_rotPlayer = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+
+	g_nCountAnimPlayer = 0;
+	g_nPatternAnimPlayer = 0;
+
+	// 頂点情報の作成
+	MakeVertexPlayer();
+
+	// テクスチャの読み込み (*33)
+	D3DXCreateTextureFromFile( pDevice, // デバイスのポインタ
+		TEXTURE_GAME_PLAYER00,            // ファイルの名前
+		&g_pD3DTexturePlayer );          // 読み込むメモリのポインタ
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void UninitPlayer(void)
+{
+	if( g_pD3DTexturePlayer != NULL ) // (*31)
+	{ // テクスチャの開放
+		g_pD3DTexturePlayer->Release();
+		g_pD3DTexturePlayer = NULL;
+	}
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void UpdatePlayer(void)
+{
+
+	SetVertexPlayer();
+
+	g_nCountAnimPlayer++;
+	// リセットカウンター
+	if (g_nCountAnimPlayer > TIME_ANIMATION-1)
+	{
+		g_nCountAnimPlayer = 0; // 先頭に戻す
+	}
+	// アニメーション (*34)
+	if( ( g_nCountAnimPlayer % TIME_ANIMATION ) == 0 )
+	{
+		// パターンの切り替え
+		g_nPatternAnimPlayer = ( g_nPatternAnimPlayer + 1 ) % ANIM_PATTERN_NUM;
+		// テクスチャ座標を設定
+		SetTexturePlayer( g_nPatternAnimPlayer );
+	}
+
+
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void DrawPlayer(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+
+	// 頂点フォーマットの設定
+	pDevice->SetFVF(FVF_VERTEX_2D);
+
+	// テクスチャの設定 (*33)
+	pDevice->SetTexture( 0, g_pD3DTexturePlayer );
+
+	// ポリゴンの描画
+	pDevice->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, NUM_PLAYER, g_vertexWkPlayer, sizeof(VERTEX_2D));
+}
+
+//=============================================================================
+// 頂点の作成
+//=============================================================================
+HRESULT MakeVertexPlayer(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+
+	// 頂点座標の設定
+	SetVertexPlayer();
+
+	// rhwの設定
+	g_vertexWkPlayer[0].rhw =
+	g_vertexWkPlayer[1].rhw =
+	g_vertexWkPlayer[2].rhw =
+	g_vertexWkPlayer[3].rhw = 1.0f;
+
+	// 反射光の設定 (*33)
+	g_vertexWkPlayer[0].diffuse = D3DCOLOR_RGBA(255, 255, 255, 0);
+	g_vertexWkPlayer[1].diffuse = D3DCOLOR_RGBA(255, 255, 255, 0);
+	g_vertexWkPlayer[2].diffuse = D3DCOLOR_RGBA(255, 255, 255, 0);
+	g_vertexWkPlayer[3].diffuse = D3DCOLOR_RGBA(255, 255, 255, 0);
+
+	// テクスチャ座標の設定 (*33)
+	g_vertexWkPlayer[0].tex = D3DXVECTOR2( 0.0f, 0.0f );
+	g_vertexWkPlayer[1].tex = D3DXVECTOR2( 1.0f/TEXTURE_PATTERN_DIVIDE_X, 0.0f );
+	g_vertexWkPlayer[2].tex = D3DXVECTOR2( 0.0f, 1.0f/TEXTURE_PATTERN_DIVIDE_Y );
+	g_vertexWkPlayer[3].tex = D3DXVECTOR2( 1.0f/TEXTURE_PATTERN_DIVIDE_X, 1.0f/TEXTURE_PATTERN_DIVIDE_Y );
+
+
+	return S_OK;
+}
+
+//=============================================================================
+// テクスチャ座標の設定 (*34)
+//=============================================================================
+void SetTexturePlayer( int cntPattern )
+{
+	// テクスチャ座標の設定
+	int x             = cntPattern % TEXTURE_PATTERN_DIVIDE_X;
+	int y             = cntPattern / TEXTURE_PATTERN_DIVIDE_X;
+	float sizeX       = 1.0f / TEXTURE_PATTERN_DIVIDE_X;
+	float sizeY       = 1.0f / TEXTURE_PATTERN_DIVIDE_Y;
+
+		g_vertexWkPlayer[0].tex = D3DXVECTOR2( (float)( x ) * sizeX, (float)( y ) * sizeY );
+		g_vertexWkPlayer[1].tex = D3DXVECTOR2( (float)( x ) * sizeX + sizeX, (float)( y ) * sizeY );
+		g_vertexWkPlayer[2].tex = D3DXVECTOR2( (float)( x ) * sizeX, (float)( y ) * sizeY + sizeY );
+		g_vertexWkPlayer[3].tex = D3DXVECTOR2( (float)( x ) * sizeX + sizeX, (float)( y ) * sizeY + sizeY );
+}
+
+// 頂点座標の設定
+void SetVertexPlayer(void) {
+	g_vertexWkPlayer[0].vtx = D3DXVECTOR3(g_posPlayer.x, g_posPlayer.y, 0.0f);
+	g_vertexWkPlayer[1].vtx = D3DXVECTOR3(g_posPlayer.x + TEXTURE_PLAYER00_SIZE_X, g_posPlayer.y, 0.0f);
+	g_vertexWkPlayer[2].vtx = D3DXVECTOR3(g_posPlayer.x, g_posPlayer.y + TEXTURE_PLAYER00_SIZE_Y, 0.0f);
+	g_vertexWkPlayer[3].vtx = D3DXVECTOR3(g_posPlayer.x + TEXTURE_PLAYER00_SIZE_X, g_posPlayer.y + TEXTURE_PLAYER00_SIZE_Y, 0.0f);
+}
