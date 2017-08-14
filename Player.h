b8f7acc7d4e397b62867f3552ffabc2d0731881c
HaijianLiu <haijanliu@gmail.com>
2017-08-14 13:36:56 +0900

    bug

---
+++ Player.h
@@ -0,0 +1,28 @@
+﻿
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+class Animation;
+
+class Player {
+private:
+	LPDIRECT3DDEVICE9 device;
+	Vertex2D vertex[4];
+
+	D3DXVECTOR3 position;
+	D3DXVECTOR3 rotation;
+
+	LPDIRECT3DTEXTURE9 texture;
+	Animation* animRun;
+
+	void SetVertex(void);
+
+public:
+	Player ();
+	virtual ~Player ();
+	void Start();
+	void Update();
+	void Draw();
+};
+
+#endif
