064b4fdc4a4f3ac68670aa6e6fffcc46403445fa
HaijianLiu <haijanliu@gmail.com>
2017-08-17 01:48:31 +0900

    shore

--- Transform.cpp
+++ Transform.cpp
@@ -1,6 +1,5 @@
 ﻿
 #include "Engine.h"
-#include "Transform.h"
 
 Transform::Transform () {
 
@@ -10,8 +9,19 @@ Transform::~Transform () {
 }
 
 void Transform::Update(Vertex2D* vertex, int spriteX, int spriteY) {
-	(vertex+0)->vertex = D3DXVECTOR3(this->position.x - (float)spriteX / 2.0f, this->position.y - (float)spriteY / 2.0f, 0.0f);
-	(vertex+1)->vertex = D3DXVECTOR3(this->position.x + (float)spriteX / 2.0f, this->position.y - (float)spriteY / 2.0f, 0.0f);
-	(vertex+2)->vertex = D3DXVECTOR3(this->position.x - (float)spriteX / 2.0f, this->position.y + (float)spriteY / 2.0f, 0.0f);
-	(vertex+3)->vertex = D3DXVECTOR3(this->position.x + (float)spriteX / 2.0f, this->position.y + (float)spriteY / 2.0f, 0.0f);
+	float sizeX = (float)(spriteX * PIXEL_SCALE) / 2.0f;
+	float sizeY = (float)(spriteY * PIXEL_SCALE) / 2.0f;
+	(vertex+0)->vertex = D3DXVECTOR3(this->position.x - sizeX, this->position.y - sizeY, 0.0f);
+	(vertex+1)->vertex = D3DXVECTOR3(this->position.x + sizeX, this->position.y - sizeY, 0.0f);
+	(vertex+2)->vertex = D3DXVECTOR3(this->position.x - sizeX, this->position.y + sizeY, 0.0f);
+	(vertex+3)->vertex = D3DXVECTOR3(this->position.x + sizeX, this->position.y + sizeY, 0.0f);
+}
+
+void Transform::Update(Vertex2D* vertex, Int2D size) {
+	float sizeX = (float)(size.x * PIXEL_SCALE) / 2.0f;
+	float sizeY = (float)(size.y * PIXEL_SCALE) / 2.0f;
+	(vertex+0)->vertex = D3DXVECTOR3(this->position.x - sizeX, this->position.y - sizeY, 0.0f);
+	(vertex+1)->vertex = D3DXVECTOR3(this->position.x + sizeX, this->position.y - sizeY, 0.0f);
+	(vertex+2)->vertex = D3DXVECTOR3(this->position.x - sizeX, this->position.y + sizeY, 0.0f);
+	(vertex+3)->vertex = D3DXVECTOR3(this->position.x + sizeX, this->position.y + sizeY, 0.0f);
 }
