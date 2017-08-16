cc2bf76ec30467e9ee81dd9914b64d544b7a48c5
HaijianLiu <haijanliu@gmail.com>
2017-08-17 01:55:30 +0900

    add: Ground class

---
+++ Ground.h
@@ -0,0 +1,20 @@
+﻿
+#ifndef _GROUND_H_
+#define _GROUND_H_
+
+class Ground {
+private:
+	Vertex2D vertex[4] = {Vertex2D()};
+	Transform* transform;
+
+	Sprite* sprite;
+
+public:
+	Ground();
+	virtual ~Ground();
+	void Start();
+	void Update();
+	void Draw();
+};
+
+#endif
