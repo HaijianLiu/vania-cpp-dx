c682cd134744088db63fbae46f8c70ebcc504c89
HaijianLiu <haijanliu@gmail.com>
2017-08-17 01:53:14 +0900

    add: struct Slice . add: new function

--- Sprite.cpp
+++ Sprite.cpp
@@ -4,9 +4,13 @@
 Sprite::Sprite(int imageSizeX, int imageSizeY, int divideX, int divideY) {
 	this->imageSize = Int2D(imageSizeX, imageSizeY);
 	this->divide = Int2D(divideX, divideY);
-	this->spriteSize = Int2D(imageSizeX / divideX * PIXEL_SCALE, imageSizeY / divideY * PIXEL_SCALE);
+	this->spriteSize = Int2D(imageSizeX / divideX, imageSizeY / divideY);
 	this->spriteMax = divideX * divideY;
 }
+Sprite::Sprite(int imageSizeX, int imageSizeY) {
+	this->imageSize = Int2D(imageSizeX, imageSizeY);
+}
+
 Sprite::~Sprite() {
 	if (this->texture != NULL) {
 		this->texture->Release();
@@ -22,6 +26,10 @@ void Sprite::CreatTexture(const char* path) {
 		1, 0,
 		D3DFMT_A8R8G8B8, D3DPOOL_MANAGED, D3DX_FILTER_NONE, D3DX_FILTER_NONE, 0xFF000000, NULL, NULL,
 		&this->texture);
+}
+
+void Sprite::MakeSlice(const char* name, int x, int y, int w, int h) {
+	this->slices.insert(std::make_pair(name, Slice(x,y,w,h)));
 }
 
 void Sprite::SetTexture(Vertex2D* vertex, int currentSprite) {
@@ -41,6 +49,25 @@ void Sprite::SetTexture(Vertex2D* vertex, int currentSprite) {
 		(vertex+1)->texCoord = D3DXVECTOR2((float)x * sizeX, (float)y * sizeY);
 		(vertex+2)->texCoord = D3DXVECTOR2((float)x * sizeX + sizeX, (float)y * sizeY + sizeY);
 		(vertex+3)->texCoord = D3DXVECTOR2((float)x * sizeX, (float)y * sizeY + sizeY);
+	}
+}
+void Sprite::SetTexture(Vertex2D* vertex, const char* name) {
+	float x = (float)this->slices.at(name).position.x / (float)this->imageSize.x;
+	float y = (float)(this->imageSize.y - this->slices.at(name).position.y - this->slices.at(name).size.y) / (float)this->imageSize.y;
+	float w = (float)this->slices.at(name).size.x / (float)this->imageSize.x;
+	float h = (float)this->slices.at(name).size.y / (float)this->imageSize.y;;
+
+	if (this->flipX == false) {
+		(vertex+0)->texCoord = D3DXVECTOR2(x,y);
+		(vertex+1)->texCoord = D3DXVECTOR2(x+w,y);
+		(vertex+2)->texCoord = D3DXVECTOR2(x,y+h);
+		(vertex+3)->texCoord = D3DXVECTOR2(x+w,y+h);
+	}
+	if (this->flipX == true) {
+		(vertex+0)->texCoord = D3DXVECTOR2(x+w,y);
+		(vertex+1)->texCoord = D3DXVECTOR2(x,y);
+		(vertex+2)->texCoord = D3DXVECTOR2(x+w,y+h);
+		(vertex+3)->texCoord = D3DXVECTOR2(x,y+h);
 	}
 }
 
--- Sprite.h
+++ Sprite.h
@@ -2,6 +2,16 @@
 #ifndef _SPRITE_H_
 #define _SPRITE_H_
 
+struct Slice {
+	Int2D position;
+	Int2D size;
+	Slice() {};
+	Slice(int x, int y, int w, int h) {
+		this->position = Int2D(x,y);
+		this->size = Int2D(w,h);
+	}
+};
+
 class Sprite {
 private:
 	LPDIRECT3DDEVICE9 device;
@@ -13,13 +23,19 @@ public:
 	Int2D spriteSize;
 	Int2D divide;
 	int spriteMax;
+
+	std::map<const char*, Slice> slices;
+
 	bool flipX = false;
 
 	Sprite(int imageSizeX, int imageSizeY, int divideX, int divideY);
+	Sprite(int imageSizeX, int imageSizeY);
 	virtual ~Sprite();
 
 	void CreatTexture(const char* path);
+	void MakeSlice(const char* name, int x, int y, int w, int h);
 	void SetTexture(Vertex2D* vertex, int currentSprite);
+	void SetTexture(Vertex2D* vertex, const char* name);
 	void Draw(Vertex2D* vertex);
 };
 
