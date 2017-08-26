44e8d3e2c3e4f11720f45c193b853b20285a1e71
HaijianLiu <haijanliu@gmail.com>
2017-08-26 12:09:28 +0900

    Lock camera to range

--- Camera.cpp
+++ Camera.cpp
@@ -11,4 +11,16 @@ Camera::~Camera() {
 void Camera::Update() {
 	this->position.x = this->target->transform->position.x;
 	this->position.y = this->target->transform->position.y;
+	if (this->position.x <= this->range[0]->transform->position.x) {
+		this->position.x = this->range[0]->transform->position.x;
+	}
+	if (this->position.x > this->range[1]->transform->position.x) {
+		this->position.x = this->range[1]->transform->position.x;
+	}
+	if (this->position.y < this->range[0]->transform->position.y) {
+		this->position.y = this->range[0]->transform->position.y;
+	}
+	if (this->position.y > this->range[1]->transform->position.y) {
+		this->position.y = this->range[1]->transform->position.y;
+	}
 }
