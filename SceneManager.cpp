d8d3f16142e4838b1506198d4905e6aa28541184
HaijianLiu <haijanliu@gmail.com>
2017-08-26 12:52:06 +0900

    add: class SceneManager

---
+++ SceneManager.cpp
@@ -0,0 +1,9 @@
+﻿
+#include "Engine.h"
+
+SceneManager::SceneManager() {
+
+}
+SceneManager::~SceneManager() {
+
+}
---
+++ SceneManager.h
@@ -0,0 +1,17 @@
+﻿
+#ifndef _SCENE_MANAGER_H_
+#define _SCENE_MANAGER_H_
+
+class SceneManager {
+private:
+	/* data */
+
+public:
+	
+
+
+	SceneManager();
+	~SceneManager();
+};
+
+#endif
